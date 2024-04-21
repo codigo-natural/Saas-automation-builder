27909dbd492ea8081a534d3b53ca71d24731a9c9
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-04-20 20:49:41 -0500

    feat implement editor provider

--- src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
@@ -1,11 +1,15 @@
-type Props = {}
+import EditorProvider from "@/providers/editor-provider";
+
+type Props = {};
 
 const page = (props: Props) => {
   return (
     <div className="h-full">
-      
+      <EditorProvider>
+        <div></div>
+      </EditorProvider>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
