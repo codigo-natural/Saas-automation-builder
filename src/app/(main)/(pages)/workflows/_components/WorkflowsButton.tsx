3e52274b5a08ff0737c273f7a66644fe1b36bcc5
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-04-20 19:51:17 -0500

    feat design workflows page

--- src/app/(main)/(pages)/workflows/_components/WorkflowsButton.tsx
+++ src/app/(main)/(pages)/workflows/_components/WorkflowsButton.tsx
@@ -1,23 +1,25 @@
 "use client";
 
+import CustomModal from "@/components/global/CustomModal";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/providers/modal-provider";
 import { Plus } from "lucide-react";
+import { Workflowform } from "@/components/forms/Workflowform"
 
 type Props = {};
 
 export function WorkflowsButton(props: Props) {
   const { setOpen, setClose } = useModal()
-  
+
   const handleClick = () => {
-    // setOpen(
-    //   <CustomModal
-    //     title="Create a workflow Automation"
-    //     subheading="Workflows are a powerfull that help you automate tasks."
-    //   >
-    //     <Workflowform />
-    //   </CustomModal>
-    // );
+    setOpen(
+      <CustomModal
+        title="Create a workflow Automation"
+        subheading="Workflows are a powerfull that help you automate tasks."
+      >
+        <Workflowform />
+      </CustomModal>
+    );
   };
 
   return (
---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className="h-full">
+      
+    </div>
+  )
+}
+
+export default page
